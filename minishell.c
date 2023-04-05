bb95a29b55c26d631c5f832b3e3d95080c3c2f6d
pealexan <pealexan@student.42porto.com>
2023-04-05 10:00:07 +0100

    added struct

--- minishell.c
+++ minishell.c
@@ -1,32 +1,38 @@
 #include "minishell.h"
 
+void	get_envvariables(t_env *env)
+{
+	int	i;
+
+	i = 0;	
+	env->user = getenv("USER");
+	env->home = getenv("HOME");
+	env->local = getenv("SESSION_MANAGER");
+	while (*env->local != '/')
+		env->local++;
+	env->local++;
+	while (env->local[i] != '.')
+		i++;
+	env->local[i] = '\0';
+}
+
 int	main(int ac, char **av, char **env)
 {
+	t_env	envvar;
 	char	*cwd;
-	char	*home;
-	char	*user;
-	char	*local;
 	char	*input;
 	char	*buffer;
 	char	*trimcwd;
 	int		i = 0;
 
-	user = getenv("USER");
-	home = getenv("HOME");
-	local = getenv("SESSION_MANAGER");
-	while (*local != '/')
-		local++;
-	local++;
-	while (local[i] != '.')
-		i++;
-	local[i] = '\0';
+	get_envvariables(&envvar);
 	while (1)
 	{
     	cwd = getcwd(0, 0);
-		while (cwd[i] == home[i])
+		while (cwd[i] == envvar.home[i])
 			i++;
 		trimcwd = ft_strdup(cwd + i);
-    	printf("%s@%s:~%s ", user, local, trimcwd);
+    	printf("%s@%s:~%s ", envvar.user, envvar.local, trimcwd);
     	buffer = readline("$");
 		printf("%s", buffer);
 		free(buffer);
