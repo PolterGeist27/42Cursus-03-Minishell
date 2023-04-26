043211b5c9be2b1ca359d7749c6718d31ea6fd04
palzap <palcleal@gmail.com>
2023-04-26 08:36:09 +0100

    added error message for no support of noclobber

--- error_handling2.c
+++ error_handling2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_handling2.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pealexan <pealexan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pealexan <pealexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 08:57:44 by pealexan          #+#    #+#             */
-/*   Updated: 2023/04/24 09:03:53 by pealexan         ###   ########.fr       */
+/*   Updated: 2023/04/26 08:34:55 by pealexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ int	unexpected_token_redir(char *input, int *i)
 			(*i)++;
 	if (input[*i - 1] == '<' && input[*i] == '|')
 		return (syntax_error_token(UNTOKEN, '|', 0));
+	else if (input[*i - 1] == '>' && input[*i] == '|')
+		return (syntax_error_operator(NOSUPPORT, ">|"));
 	else if (ft_strrchr(REDIR, input[*i]) && input[*i] != input[*i + 1])
 		return (syntax_error_token(UNTOKEN, input[*i], 0));
 	else if (ft_strrchr(REDIR, input[*i]) && !input[*i])
